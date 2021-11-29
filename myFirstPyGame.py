0b0b29240ccac29e2a756a6380265e8d797e9f1a
Ashley W <90473515+s11806981duvalschools@users.noreply.github.com>
2021-11-29 14:51:58 -0500

    v0.3
    Filled background color and drew a polygon onto the screen.

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Ashley Wright, 11/29/21, 2:41pm, v0.2
+# My First PyGame, Ashley Wright, 11/29/21, 2:50pm, v0.3
 
 import pygame, sys 
 from pygame.locals import * 
@@ -25,3 +25,9 @@ text = basicFont.render('Hello, world', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
+
+# Fill background color.
+windowSurface.fill(WHITE)
+
+# Draw a polygon onto the screen.
+pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106),(236, 277), (56, 277), (0, 106)))
