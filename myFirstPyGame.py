1510267f528c26864022f2cf793b9e4cfe38751c
Ashley W <90473515+s11806981duvalschools@users.noreply.github.com>
2021-11-29 14:44:28 -0500

    v0.2
    Setup font, created text rectangle.

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Ashley Wright, 11/29/21, 2:25pm, v0.1
+# My First PyGame, Ashley Wright, 11/29/21, 2:41pm, v0.2
 
 import pygame, sys 
 from pygame.locals import * 
@@ -16,3 +16,12 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup font.
+basicfont = pygame.font.SysFont(None, 48)
+
+# Setup text.
+text = basicFont.render('Hello, world', True, WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
